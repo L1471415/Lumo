91bd88b14770602586efff18df12d49ea506be56
LukeS26 <lukesumma@proton.me>
2024-03-30 03:07:25 -0400

    added enumerate

--- config/users.py
+++ config/users.py
@@ -22,12 +22,12 @@ class Users:
         return self.users[id]
 
     def get_id_from_number(self, number:str):
-        for user_id, user in self.users:
+        for user_id, user in enumerate(self.users):
             if user.number == number:
                 return user_id
         
     def contains_number(self, number:str):
-        for user_id, user in self.users:
+        for user_id, user in enumerate(self.users):
             if user.number == number:
                 return True
 
